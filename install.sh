47406f38bd6f7665b9afca1a9a9e2588b6d0e09e
Shen Chen <shenchen@cogenda.com>
2014-01-06 14:11:29 +0800

    better detection of lyx user dir in install script

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 TEXMFHOME=$(kpsewhich -var-value TEXMFHOME)
-echo -n "User LaTeX templates are to be installed to $TEXMFHOME ... "
+echo -n "User LaTeX templates are to be installed to $TEXMFHOME         ... "
 DSTDIR=$TEXMFHOME/tex/latex
 [[ -d $DSTDIR ]] || mkdir -p $DSTDIR
 cp -r src/tex/latex/cgdrep $DSTDIR
 echo "Done"
 
-if [[ -d  $HOME/.lyx ]]
+FOUND=0
+for lyxdir in .lyx2.1 .lyx2.0 .lyx
+do
+    if [[ -d  $HOME/$lyxdir ]]
+    then
+        FOUND=1
+        echo -n "Lyx user dir found in $HOME/$lyxdir, installing Lyx layouts       ... "
+        cp src/lyx/layouts/* $HOME/$lyxdir/layouts/
+        echo "Done"
+    fi
+done
+if [[ $FOUND -eq 0 ]]
 then
-  echo -n "Lyx found, installing Lyx layouts ... "
-  cp src/lyx/layouts/* $HOME/.lyx/layouts/
-  echo "Done"
-else
   echo "Lyx not found, is it installed?"
 fi
 
