4fda549adfa8331a813504dbba7499905f58fc42
Shen Chen <shenchen@cogenda.com>
2014-09-07 22:01:02 +0800

    detect available ttf fonts in install scripts.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+##################### Part 1 : Latex class  #########################
 TEXMFHOME=$(kpsewhich -var-value TEXMFHOME)
 echo -n "User LaTeX templates are to be installed to $TEXMFHOME         ... "
 DSTDIR=$TEXMFHOME/tex/latex
@@ -7,6 +8,7 @@ DSTDIR=$TEXMFHOME/tex/latex
 cp -r src/tex/latex/cgdrep $DSTDIR
 echo "Done"
 
+##################### Part 2 : Lyx layout   #########################
 FOUND=0
 for lyxdir in .lyx2.1 .lyx2.0 .lyx
 do
@@ -18,8 +20,39 @@ do
         echo "Done"
     fi
 done
-if [[ $FOUND -eq 0 ]]
-then
-  echo "Lyx not found.  If you don't need lyx, neglect this line."
-fi
+
+
+##################### Part 3 : Font setting #########################
+
+setFont() {
+    fname=$DSTDIR/cgdrep/$1
+    setcmd=$2
+    declare -a fontLst=("${!3}")
+
+    echo "checking $setcmd fonts"
+
+    for (( i = 0 ; i < ${#fontLst[@]} ; i++ ))
+    do
+        font=${fontLst[$i]}
+
+        fc-list -q "$font"
+
+        if [[ $? -eq 0 ]]
+        then
+            echo "... found $font"
+            sed -i -e 's/\\$setcmd/\\$setcmd\{$font\}/' $fname
+            break
+        else
+            echo "... can't find $font"
+        fi
+    done
+}
+
+MONOFONTS=("M+ 1m regular"
+           "Dejavu Sans Mono"
+           "Latin Modern Mono"
+          )
+
+setFont cgd-font-en.def setmonofont MONOFONTS[@]
+
 
