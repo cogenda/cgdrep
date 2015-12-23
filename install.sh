316d7b58928abdcccd3dc31509533bf2e33032ac
shenchen <shenchen@cogenda.com>
2015-12-23 17:09:44 +0800

    support global install

--- install.sh
+++ install.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
+# USAGE:
+# 1. install to user's local directories
+#
+#   $ ./install
+#
+# 2. install to global directories
+#
+#   $ sudo -sH
+#   $ ./install --global
+#
+
+if [[ $1 == '--global' ]];
+then
+    TEXMF=$(kpsewhich -var-value TEXMFLOCAL)
+    LYXDIR=$(cd $(dirname $(which lyx)) && cd .. && pwd)/share/lyx
+else
+    TEXMF=$(kpsewhich -var-value TEXMFHOME)
+    LYXDIR=
+    for lyxdir in .lyx2.1 .lyx
+    do
+        if [[ -d  "$HOME/$lyxdir" ]]
+        then
+            LYXDIR="$HOME/$lyxdir"
+        fi
+    done
+fi
+
 ##################### Part 1 : Latex class  #########################
-TEXMFHOME=$(kpsewhich -var-value TEXMFHOME)
-echo -n "User LaTeX templates are to be installed to $TEXMFHOME         ... "
-DSTDIR=$TEXMFHOME/tex/latex
+echo -n "User LaTeX templates are to be installed to $TEXMF         ... "
+DSTDIR=$TEXMF/tex/latex
 [[ -d $DSTDIR ]] || mkdir -p $DSTDIR
 cp -r src/tex/latex/cgdrep $DSTDIR
 echo "Done"
 
 ##################### Part 2 : Lyx layout   #########################
-FOUND=0
-for lyxdir in .lyx2.1 .lyx2.0 .lyx
-do
-    if [[ -d  $HOME/$lyxdir ]]
-    then
-        FOUND=1
-        echo -n "Lyx user dir found in $HOME/$lyxdir, installing Lyx layouts       ... "
-        cp src/lyx/layouts/* $HOME/$lyxdir/layouts/
-        echo "Done"
-    fi
-done
+if [[ -d $LYXDIR ]]
+then
+    echo -n "Lyx user dir found in $LYXDIR, installing Lyx layouts       ... "
+    cp src/lyx/layouts/* $LYXDIR/layouts/
+    echo "Done"
+fi
 
 
 ##################### Part 3 : Font setting #########################
